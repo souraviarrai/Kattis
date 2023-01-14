96ac1df83ecf1addfdd28e9c7eb1256a01f75a8b
SOURAV RAI <12190085.gcit@rub.edu.bt>
2023-01-15 03:55:34 +0600

    new

--- easy/listquestion.py/question5.py
+++ easy/listquestion.py/question5.py
@@ -1,1 +1,10 @@
 # interchanging first item and last item in a list
+
+
+
+def swap(newlist):
+    newlist[0],newlist[-1] = newlist[-1],newlist[0]
+    return newlist
+
+my_list = [1,2,3,4,5,6,9]
+print(swap(my_list))
